80042468cd9507fc5e642c5b62dc2491882e06e4
Moises Elias R.C. <141798007+Sesiom31@users.noreply.github.com>
2024-05-21 16:10:39 -0500

    Config de redux & redux-toolkit, store y authSlice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 
+
 function App() {
   return (
     <BrowserRouter>
